da50b14c71b59b3228d267227cfd370f45046f71
Marija Ilić <marija.cbsqg@gmail.com>
2024-12-22 15:38:47 +0100

    refactor

--- src/services/backendApiService.js
+++ src/services/backendApiService.js
@@ -1,13 +1,13 @@
-import { keys, storage } from "./storageService";
+import { getAuthToken } from "./authService";
 
 const backendUrl = import.meta.env.VITE_MEDIA_APP_URL;
 
 const trimUrl = (url) => (url[0] === "/" ? url.slice(1) : url);
 
 const getAuthHeaders = () => {
-    const tokenData = storage.get(keys.TOKEN);
-    const tokenType = tokenData ? JSON.parse(tokenData).token_type : null;
-    const accessToken = tokenData ? JSON.parse(tokenData).access_token : null;
+    const tokenData = getAuthToken();
+    const tokenType = tokenData ? tokenData.token_type : null;
+    const accessToken = tokenData ? tokenData.access_token : null;
 
     return tokenType && accessToken
         ? { Authorization: `${tokenType} ${accessToken}` }
